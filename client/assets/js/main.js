a517d11d6755e9f1d8486f82bed70dc8574b29a7
Liban Mohamed Yusuf <88878347+liibaanpro@users.noreply.github.com>
2023-02-09 09:42:06 +0300

    Update main.js

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -100,7 +100,7 @@ const handleSubmit = async (e) => {
   } else {
     const err = await response.text();
 
-    messageDiv.innerHTML = "Something went wrong";
+    messageDiv.innerHTML = "Wax baa qaldamay !";
 
     alert(err);
   }
