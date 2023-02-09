c992eaf051ab71c35fd87ea3956955e25a7d92f2
Liban Mohamed Yusuf <88878347+liibaanpro@users.noreply.github.com>
2023-02-09 08:02:27 +0300

    Update main.js

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from server -> bot's response
 
-  const response = await fetch('https://somali.onrender.com', {
+  const response = await fetch('https://somali.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
